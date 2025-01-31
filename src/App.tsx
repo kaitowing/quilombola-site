3cbe77307d88118a07660dccfc428a34b42425fc
kaitowing <pizzolatti.arthur@edu.pucrs.br>
2025-01-31 18:29:16 -0300

    center download buttons and update Android download link

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
 
           <div className="download-buttons">
             <a
-              href="#android-download"
+              href="/quilombola-site/Quilombytes.apk "
               className="download-btn android"
               aria-label="Baixar para Android"
             >
